31d5a17b7155a2a05e81cb31af67e6cddf731878
Nazemnykh Anton <nazemnykh.anton@gmail.com>
2015-04-15 10:12:42 +0500

    По аналогии с SocketCanLib создана SocketJ1939Lib.

---
+++ src/SocketJ1939Lib.c
@@ -0,0 +1,227 @@
+#define _GNU_SOURCE
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
+#include <net/if.h>
+#include <linux/can.h>
+#include <linux/can/j1939.h>
+#include <linux/can/error.h>
+#include <unistd.h>
+
+#include <stdio.h>
+
+#include "SocketJ1939Lib.h"
+
+int SocketOpen (char *InterfaceName, int TxBuffSize, int RxBuffSize)
+{
+    int number;
+
+    // Создаем висящий сокет
+    number = socket(PF_CAN, SOCK_DGRAM, CAN_J1939);
+    if(number < 0)
+    {
+        int errsv = errno;
+        return -100*errsv;
+    }
+
+    const int disable = 0, enable = 1; // Для использования в setsockopt
+
+    // Приём всех сообщений (не только адресованных нам)
+    if ( setsockopt(number, SOL_CAN_J1939, SO_J1939_PROMISC, &enable, sizeof(int)) < 0 )
+        return -10;
+
+    // Включаем приём собственных сообщений
+    if ( setsockopt(number, SOL_CAN_J1939, SO_J1939_RECV_OWN, &enable, sizeof(int)) < 0 )
+        return -11;
+    // Нужно для того, чтобы не вырезался родитель skb->sk,
+    // по которому происходит определение своего пакета
+    if ( setsockopt(number, SOL_SOCKET, SO_TIMESTAMPING, &enable, sizeof(int)) < 0 )
+        return -12;
+
+    // Timestamp
+    if ( setsockopt(number, SOL_SOCKET, SO_TIMESTAMP, &enable, sizeof(int)) < 0 )
+        return -13;
+
+    // Размеры буферов
+    if ( setsockopt(number, SOL_SOCKET, SO_SNDBUF, &TxBuffSize, sizeof(TxBuffSize)) < 0 )
+        return -14;
+    if ( setsockopt(number, SOL_SOCKET, SO_RCVBUF, &RxBuffSize, sizeof(RxBuffSize)) < 0 )
+        return -15;
+
+    // Запрос переполнения
+    if ( setsockopt(number, SOL_SOCKET, SO_RXQ_OVFL, &enable, sizeof(enable)) < 0 )
+        return -16;
+
+    // Биндим сокет на нужный интерфейс
+
+    // Определяем индекс интерфейса
+    struct ifreq ifr;
+    strcpy(ifr.ifr_name, InterfaceName);
+
+    // Полученный индекс будет записан в поле ifr.ifr_ifindex
+    ioctl(number, SIOCGIFINDEX, &ifr);
+    if(ifr.ifr_ifindex < 0)
+    {
+        int errsv = errno;
+        return -10000*errsv;
+    }
+
+    struct sockaddr_can addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.can_family = AF_CAN;
+    addr.can_ifindex = ifr.ifr_ifindex;
+    addr.can_addr.j1939.name = J1939_NO_NAME;
+    addr.can_addr.j1939.addr = J1939_NO_ADDR;
+    addr.can_addr.j1939.pgn = J1939_NO_PGN;
+    if ( bind(number, (struct sockaddr*)&addr, sizeof(addr)) != 0 )
+    {
+        int errsv = errno;
+        return -1000000*errsv;
+    }
+
+    return number;
+}
+
+int SocketClose (int number)
+{
+    if ( close (number) != 0)
+    {
+        int errsv = errno;
+        return -1*errsv;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int SocketRead (int Socket, J1939FrameBag *Bags, unsigned int BagsCount, int TimeoutMs)
+{
+    struct mmsghdr msgs[BagsCount];
+    struct iovec iovs[BagsCount];
+    struct sockaddr_can addr[BagsCount];
+    char ctrlmsgs[BagsCount][(CMSG_SPACE(sizeof(struct timeval)) + CMSG_SPACE(sizeof(__u32)))];
+
+    unsigned int i;
+    for (i = 0; i < BagsCount; i++)
+    {
+        memset(&msgs[i], 0, sizeof(msgs[0]));
+        memset(&iovs[i], 0, sizeof(iovs[0]));
+        memset(&addr[i], 0, sizeof(addr[0]));
+
+        msgs[i].msg_hdr.msg_name = &addr[i];
+        msgs[i].msg_hdr.msg_namelen = sizeof(struct sockaddr_can);
+
+        iovs[i].iov_base = (void *) &(Bags[i].Frame.data);
+        iovs[i].iov_len = sizeof(Bags[i].Frame.data);
+        msgs[i].msg_hdr.msg_iov = &iovs[i];
+        msgs[i].msg_hdr.msg_iovlen = 1;
+
+        msgs[i].msg_hdr.msg_control = &ctrlmsgs[i];
+        msgs[i].msg_hdr.msg_controllen = sizeof(ctrlmsgs[0]);
+        msgs[i].msg_hdr.msg_flags = 0;
+    }
+
+    int rcount;
+    rcount = recvmmsg(Socket, msgs, BagsCount, MSG_DONTWAIT, NULL);
+    if (rcount >= 0)
+    {
+        for (i = 0; i < rcount; i ++)
+        {
+            struct timeval tv;
+            struct cmsghdr *cmsg;
+
+            for (cmsg = CMSG_FIRSTHDR(&msgs[i].msg_hdr);
+                cmsg;
+                cmsg = CMSG_NXTHDR(&msgs[i].msg_hdr,cmsg))
+            {
+                switch (cmsg->cmsg_level) {
+                case SOL_SOCKET:
+                    if (cmsg->cmsg_type == SO_TIMESTAMP)
+                    {
+                        tv = *(struct timeval *)CMSG_DATA(cmsg);
+                        Bags[i].TimeStamp.seconds = tv.tv_sec;
+                        Bags[i].TimeStamp.microseconds = tv.tv_usec;
+                    }
+                    else if (cmsg->cmsg_type == SO_RXQ_OVFL)
+                        Bags[i].DroppedMessagesCount = *(__u32 *)CMSG_DATA(cmsg);
+                    break;
+                case SOL_CAN_J1939:
+
+                    break;
+                }
+            }
+
+            Bags[i].Frame.pgn = addr[i].can_addr.j1939.pgn;
+
+            if (msgs[i].msg_hdr.msg_flags & MSG_CONFIRM)
+                Bags[i].Flags |= (1 << 0);
+        }
+        return rcount;
+    }
+    else
+    {
+        int errsv = errno;
+        if (errsv == EAGAIN)
+          return 0;
+        else
+          return errsv;
+    }
+}
+
+int SocketWrite (int Socket, J1939Frame *Frame, int FramesCount)
+{
+    struct mmsghdr msgs[FramesCount];
+    struct iovec iovs[FramesCount];
+    struct sockaddr_can addr[FramesCount];
+
+    unsigned int i;
+    for (i = 0; i < FramesCount; i++)
+    {
+        memset (msgs[i], 0, sizeof (msgs[0]));
+        memset (iovs[i], 0, sizeof (iovs[0]));
+        memset (addr[i], 0, sizeof (addr[0]));
+
+        addr[i].can_addr.j1939.name = J1939_NO_NAME;
+        addr[i].can_addr.j1939.addr = J1939_NO_ADDR;
+        addr[i].can_addr.j1939.pgn = Frame[i].pgn;
+
+        msgs[i].msg_hdr.msg_name = &addr[i];;
+        msgs[i].msg_hdr.msg_namelen = sizeof(addr[i]);
+
+        iovs[i].iov_base = (void *) &(Frame[i].data);
+        iovs[i].iov_len = sizeof(Frame[i].data);
+        msgs[i].msg_hdr.msg_iov = &iovs[i];
+        msgs[i].msg_hdr.msg_iovlen = 1;
+    }
+
+    int scount;
+    scount = sendmmsg(Socket, msgs, FramesCount, 0);
+    if ( scount < 0 )
+    {
+        int errsv = errno;
+        return -errsv;
+    }
+    else
+    {
+        return scount;
+    }
+}
+
+int SocketFlushInBuffer (int Socket)
+{
+  struct FrameBag bags[10];
+
+  int lost = 1;
+  while (lost)
+  {
+    lost = SocketRead (Socket, bags, 10, 1);
+    if (lost < 0)
+      return lost;
+  }
+  return 0;
+}
---
+++ src/SocketJ1939Lib.h
@@ -0,0 +1,64 @@
+// COPYRIGHT NPO SAUT
+// mailto: nazemnykh.anton@gmail.com
+// version: 1
+
+#include <linux/can.h>
+
+// Открывает сокет на CAN-интерфейсе с именем InterfaceName
+//  TxBuffSize и RxBuffSize задают размеры входищего и сходящего буферов в сообщениях [3 .. 465]
+//  В случае успеха возвращает неотрицательный хендлер сокета
+//  При ошибке возращает код ошибки в формате:
+//   -100*errno для ошибок в socket()
+//   -10000*errno для ошибок в ioctl()
+//   -1000000*errno для ошибов в bind()
+int SocketOpen (char *InterfaceName, int TxBuffSize, int RxBuffSize);
+
+// Закрывает открытый сокет
+//  В случае успеха возвращает 0
+//  При ошибке возвращает код ошибки: -1*errno в функции close()
+int SocketClose (int number);
+
+// FrameBag Flags
+// bit 0      : Loopback
+typedef __u8 FrameBagFlags;
+
+struct __attribute__((packed, aligned(1))) TimeVal
+{
+  __u32 seconds;
+  __u32 microseconds;
+};
+
+#define J1939MaxDataLen 1785
+struct __attribute__((packed, aligned(1))) J1939Frame
+{
+  __u32 pgn;
+  __8 data[J1939MaxDataLen];
+};
+
+struct __attribute__((packed, aligned(1))) J1939FrameBag
+{
+  struct J1939Frame Frame;
+  struct TimeVal TimeStamp;
+  FrameBagFlags Flags;
+  __u32 DroppedMessagesCount;
+};
+
+// Читает сообщения из входящего буфера сокета.
+//  Функция блокируется на время, не превыщающее TimeoutMs
+//  Читает из входящего буфера драйвера не более чем BagsCount сообщений
+//  В случае успеха возвращает количество прочитанных сообщений
+//  При ошибке - отрицательный код ошибки:
+//	-1	: Сокет закрыт
+//	-255	: Не знамо что
+int SocketRead (int Socket, struct J1939FrameBag *Bags, unsigned int BagsCount, int TimeoutMs);
+
+// Ставит сообщения в очередь SocketCan на отправку
+//  Если очередь свободна, то не блокирует ???
+//  Иначе блокируется до освобожения места в очереди ???
+//  Возвращает 1 в случае успеха и отрицательный код ошибки при ошибке ???
+int SocketWrite (int Socket, struct J1939Frame *Frame, int FramesCount);
+
+// Очищает буфер принятых сообщений сокета
+//  Функция блокирующая
+//  При успехе возвращает 0, в случае ошибки отрицательный код ошибки
+int SocketFlushInBuffer (int Socket);
