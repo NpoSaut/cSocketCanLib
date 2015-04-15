be373a1907a0d45f53c71c24395953bd117296ff
Nazemnykh Anton <nazemnykh.anton@gmail.com>
2015-04-15 15:15:06 +0500

    Исправлены "тесты" для совместимости с версией библиотеки 2.0

--- src/test_receive.c
+++ src/test_receive.c
@@ -8,21 +8,22 @@ struct timeval t1, t2;
 
 int main ()
 {
-    int s = SocketOpen ("vcan0");
+    int s = SocketOpen ("vcan0", 1000, 1000);
 
+    struct FrameBag bag;
     int yes = 0;
     gettimeofday (&t1, NULL);
     gettimeofday (&t2, NULL);
     while ( t2.tv_sec - t1.tv_sec < 1 )
     {
-        if ( SocketRead (s, &frame) > 0
-             &&frame.can_id == 0x111
-             && frame.can_dlc == 5
-             && frame.data[0] == 1
-             && frame.data[1] == 2
-             && frame.data[2] == 3
-             && frame.data[3] == 4
-             && frame.data[4] == 5 )
+        if ( SocketRead (s, &bag, 1, 700) > 0
+             && bag.Frame.can_id == 0x111
+             && bag.Frame.can_dlc == 5
+             && bag.Frame.data[0] == 1
+             && bag.Frame.data[1] == 2
+             && bag.Frame.data[2] == 3
+             && bag.Frame.data[3] == 4
+             && bag.Frame.data[4] == 5 )
         {
             yes = 1;
             break;
--- src/test_send.c
+++ src/test_send.c
@@ -4,8 +4,8 @@ struct can_frame frame = { 0x111, 5, {1,2,3,4,5,6,7,8} };
 
 int main ()
 {
-    int s = SocketOpen ("vcan0");
-    SocketWrite (s, &frame);
+    int s = SocketOpen ("vcan0", 1000, 1000);
+    SocketWrite (s, &frame, 1);
     SocketClose (s);
     return 0;
 }
