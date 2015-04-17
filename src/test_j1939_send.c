a363e84da411cab823740533e4472c54df2a1582
Nazemnykh Anton <nazemnykh.anton@gmail.com>
2015-04-17 15:30:48 +0500

    В тесте отправки J1939 убрана собственно сама отправка

--- src/test_j1939_send.c
+++ src/test_j1939_send.c
@@ -1,12 +1,11 @@
 #include "SocketJ1939Lib.h"
 
-struct J1939Frame frame = { 0x2300, {0x01, 0x02, 0xAA}  };
+struct J1939Frame frame = { 0x2300, 3, {0x01, 0x02, 0xAA}  };
 
 int main ()
 {
     int s = J1939SocketOpen ("vcan0", J1939MaxDataLen*10, J1939MaxDataLen*10);
-    printf("ret: %i\n", s);
-    s = J1939SocketWrite (s, &frame, 1);
+//    s = J1939SocketWrite (s, &frame, 1);
     printf("write ret: %i\n", s);
     J1939SocketClose (s);
     return 0;
